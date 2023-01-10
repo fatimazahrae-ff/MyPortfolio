57c6c2f2fe2389b389f6a9a9ca267601a9712880
unknown <fatiadm01@gmail.com>
2023-01-10 23:25:36 +0100

    Add Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import Header from "./components/header/Header"
+import Footer from "./components/footer/Footer"
+import Services from "./components/services/Services"
+import Nav from "./components/nav/Nav"
+import Portfolio  from "./components/portfolio/Portfolio"
+import Contact  from "./components/contact/Contact"
+import About from "./components/about/About"
+import Experience from "./components/experience/Experience"
+
 
 export default function App() {
   return (
-    <div>App</div>
+    <>
+    <Header/>
+    <Nav/>
+    <About/>
+    <Experience/>
+    <Services/>
+    <Portfolio/>
+    <Contact/>
+    <Footer/>
+    
+    </>
   )
 }
