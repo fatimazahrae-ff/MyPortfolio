89a5873574024f36bc23a9beebfd3f0e2116ceef
unknown <fatiadm01@gmail.com>
2023-01-11 01:17:21 +0100

    fixes

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CTA = () => {
+  return (
+    <div className='cta'>
+        <a href="#Test" className='btn'>Télécharger le CV</a>
+        <a href="#contact" className='btn btn-primary'>Contacter moi</a>
+    </div>
+  )
+}
+
+export default CTA
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container" >
+      <h5>Bonjour je suis </h5>
+      <h1>Fatimazahrae Jalal</h1>
+      <h5 className="text-light">Ingénieur d'état en informatique</h5>
+      <CTA/>
+      </div>
+    </header>
   )
 }
 
